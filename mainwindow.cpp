f72f594ae6e33a60ed446dce0dd1f4a7d0f810bc
Mars Cadieux <cadieuxmars@gmail.com>
2024-02-21 19:38:20 -0500

    init

---
+++ mainwindow.cpp
@@ -0,0 +1,19 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    connect(ui->pushButton, SIGNAL(released()), this, SLOT (doSomething()));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::doSomething() {
+    qInfo("hello world");
+}
