b876101c955a3181b6ae20ebdfe46afc5f6a8ec4
Mars Cadieux <cadieuxmars@gmail.com>
2024-03-10 00:31:11 -0500

    minor styling applied to main window

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9,6 +9,7 @@
 #include <QPushButton>
 #include <QSpacerItem>
 #include <QFrame>
+#include <QLabel>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -24,17 +25,30 @@ MainWindow::MainWindow(QWidget *parent)
 
     ui->setupUi(this);
 
+    layout->setColumnMinimumWidth(1, 10);
+
     for(int i=1; i<=NUM_FLOORS; ++i){
 
-        FloorButton* upButton = new FloorButton(this, i, "up");
-        layout->addWidget(upButton, 3*i -2, 0);
-        //upButton->show();
-        connect(upButton, SIGNAL(released()), this, SLOT (doSomething()));
+        QLabel* floorLabel = new QLabel(this);
+        floorLabel->setText(QString::number(i));
+        layout->addWidget(floorLabel, 3*(NUM_FLOORS -i)+1, 0, 2, 1, Qt::AlignRight);
 
-        FloorButton* downButton = new FloorButton(this, i, "down");
-        layout->addWidget(downButton, 3*i -1, 0);
-        //downButton->show();
-        connect(downButton, SIGNAL(released()), this, SLOT (doSomething()));
+        if(i != NUM_FLOORS){
+            FloorButton* upButton = new FloorButton(this, i, "up");
+            upButton->setFixedWidth(30);
+            layout->addWidget(upButton, 3*(NUM_FLOORS -i)+1, 1);
+            //upButton->show();
+            connect(upButton, SIGNAL(released()), this, SLOT (doSomething()));
+        }
+
+        if(i != 1){
+            FloorButton* downButton = new FloorButton(this, i, "down");
+            layout->addWidget(downButton, 3*(NUM_FLOORS -i) +2, 1);
+            downButton->setFixedWidth(30);
+            //downButton->show();
+            connect(downButton, SIGNAL(released()), this, SLOT (doSomething()));
+        }
+
 
         //QSpacerItem* spacer = new QSpacerItem(10, 10);
 
@@ -46,14 +60,15 @@ MainWindow::MainWindow(QWidget *parent)
             line->setFrameShadow(QFrame::Sunken);
 
             layout->setRowMinimumHeight(3*i, 20);
-            layout->addWidget(line, 3*i, 0);
+            layout->addWidget(line, 3*(NUM_FLOORS -i), 1);
         }
 
     }
 
     QPushButton* test = new QPushButton("test");
 
-    layout->addWidget(test, NUM_FLOORS*3/2, 2);
+    layout->setColumnMinimumWidth(2, 50);
+    layout->addWidget(test, NUM_FLOORS*3/2, 2, Qt::AlignRight);
 
     QWidget *window = new QWidget();
     window->setLayout(layout);
@@ -80,6 +95,10 @@ void MainWindow::on_fireButton_released()
 }
 
 void MainWindow::doSomething(){
-    qInfo("test");
+    //qInfo("test");
+    FloorButton* fButton = qobject_cast<FloorButton*>(sender());
+    int floor = fButton->getFloorNum();
+    string dir = fButton->getDirection();
+    cout<<"floor "<<floor<<" "<<dir<<" requested"<<endl;
 }
 
